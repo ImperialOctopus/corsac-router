6da0a4ef9351e6d5321b02df41477dffad72cdf5
Zoey Child <imperialoctopus@gmail.com>
2020-09-06 14:24:23 +0100

    Remove now-unnecessary all test file

--- test/all.dart
+++
@@ -1,9 +0,0 @@
-library corsac_router.test.all;
-
-import 'resource_test.dart' as resource_test;
-import 'router_test.dart' as router_test;
-
-void main() {
-  resource_test.main();
-  router_test.main();
-}
